64a452895857406d3b333ccd949d40df4d5f7757
roliiii <roli4759@hotmail.com>
2019-12-31 13:13:38 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
 import './App.css';
 
-class App extends Component {
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {counter : 0};
+    this.dec = this.dec.bind(this);
+  }
+
+
+  //this binded implicit
+  inc = () => {
+    this.setState({counter: this.state.counter + 1});
+  }
+
+  //need this to be bind
+  dec(){
+    this.setState({counter: this.state.counter - 1});
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <Fragment>
+      <label>Counter: {this.state.counter}</label>
+      <div>
+       <button type="button" onClick={this.inc} >Increase</button>
+       <button type="button" onClick={this.dec}>Decrease</button>
       </div>
+      </Fragment>
     );
   }
 }
 
-export default App;
